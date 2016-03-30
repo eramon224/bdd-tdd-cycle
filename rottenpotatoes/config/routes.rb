ccc0c16a3c0dfadd4e1efedce20240b25f2e3470
eramon224 <eramon224@tamu.edu>
2016-03-30 22:39:50 +0000

    test

--- rottenpotatoes/config/routes.rb
+++ rottenpotatoes/config/routes.rb
@@ -14,6 +14,7 @@ Rottenpotatoes::Application.routes.draw do
   #   resources :products
   resources :movies
 
+  get '/'
   # Sample resource route with options:
   #   resources :products do
   #     member do
