dae25081d4ba923c7f2d19d251f1d71924caa344
eramon224 <eramon224@tamu.edu>
2016-03-31 19:13:13 +0000

    test

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,12 +9,11 @@ class MoviesController < ApplicationController
   def similar
     id = params[:id]
     @movie = Movie.find_by_id(id)
-    if (@movie.director.length == 0)
-      flash[:notice] = "'#{@movie.title}' has no director info."
-      redirect_to movies_path
-    end
-    
-    @movies = Movie.find_all_by_director(@movie.director)
+    #if (@movie.director.length == 0)
+    #  flash[:notice] = "'#{@movie.title}' has no director info."
+    #  redirect_to movies_path
+   # end
+   # @movies = Movie.find_all_by_director(@movie.director)
   end
   
   def index
